c6ada7ba5e794f1364425b79ead97c154ba65beb
Joshua Carroll <carrolljosh1976@gmail.com>
2024-11-11 17:23:15 -0500

    Added content to PlaceItem component

--- components/Places/PlaceItem.jsx
+++ components/Places/PlaceItem.jsx
@@ -1,3 +1,17 @@
-export default function PlaceItem() {
-    
+import { Image, Pressable, Text, View } from "react-native";
+
+export default function PlaceItem({place,onSelect}) {
+    return (
+        <Pressable onPress={onSelect}>
+            <Image source={{uri: place.imageUri}} />
+            <View>
+                <Text>{place.title}</Text>
+                <Text>{place.address}</Text>
+            </View>
+        </Pressable>
+    )
 }
+
+const styles = StyleSheet.create({
+    
+})
