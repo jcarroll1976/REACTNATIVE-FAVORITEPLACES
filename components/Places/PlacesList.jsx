10d54ba4e6dcb46ef7b4cb60cf3ea4fca9450317
Joshua Carroll <carrolljosh1976@gmail.com>
2024-11-10 21:52:10 -0500

    created PlaceItem component for each individual place

--- components/Places/PlacesList.jsx
+++ components/Places/PlacesList.jsx
@@ -1,7 +1,11 @@
 import { FlatList } from "react-native";
+import PlaceItem from "./PlaceItem";
 
 export default function PlacesList({places}) {
     return (
-        <FlatList data={places} keyExtractor={(item) => item.id} renderItem={a} />
+        <FlatList 
+            data={places} 
+            keyExtractor={(item) => item.id} 
+            renderItem={(item)=> <PlaceItem place={item} />} />
     )
 }
